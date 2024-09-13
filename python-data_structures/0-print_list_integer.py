0c3837a15c85c77fcd82bb3dbe2f0970e4b737d8
ninotna <antonin@dev87.fr>
2024-09-13 11:30:24 +0200

    another fix..

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
-
 def print_list_integer(my_list=[]):
+    """
+    Prints all integers in a list, one per line.
+
+    Args:
+    my_list (list): A list of integers.
+
+    Example:
+    >>> print_list_integer([1, 2, 3])
+    1
+    2
+    3
+    """
     for element in my_list:
         print("{:d}".format(element))
