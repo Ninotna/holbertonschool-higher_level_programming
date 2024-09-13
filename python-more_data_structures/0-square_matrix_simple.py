60b387a2a96c4cc669f102fb3ac10307a5bd6443
ninotna <antonin@dev87.fr>
2024-09-13 14:53:35 +0200

    new try

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     """
     square_matrix_simple - Computes the square value of all integers
     @matrix: A 2 dimensional array of integers
-    Return: A new matrix with squared values
+    Return: A new matrix with squared values..
     """
     return [[x ** 2 for x in row] for row in matrix]
