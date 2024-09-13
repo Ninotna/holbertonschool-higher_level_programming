10fd629694ee66a177b90b915278408e1c2de66a
ninotna <antonin@dev87.fr>
2024-09-13 14:55:14 +0200

    use of map

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    """
-    square_matrix_simple - Computes the square value of all integers
-    @matrix: A 2 dimensional array of integers
-    Return: A new matrix with squared values..
-    """
-    return [[x ** 2 for x in row] for row in matrix]
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (new_matrix)
