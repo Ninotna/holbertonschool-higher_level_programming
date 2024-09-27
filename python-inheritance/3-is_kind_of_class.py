51f3ddbd1ca57934749d8f25870d99a277129b2a
ninotna <antonin@dev87.fr>
2024-09-27 14:50:46 +0200

    fixing 3 and 4

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
+"""
+Module that defines a function to check the type of an object.
+"""
+
+
 def is_kind_of_class(obj, a_class):
     """
-    is_kind_of_class - returns True if object is an instance of, or if
-    the object is an instance of a class that inherited from, the specified class
-    @obj: the object to check
-    @a_class: the class or parent class to match the type of obj to
-    Return: True if obj is an instance or inherited instance of a_class, else False
+    Checks if the object is an instance of, or if the object is an instance
+    of a class that inherited from, the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to match the type of obj against.
+
+    Returns:
+        True if obj is an instance of a_class or a subclass of a_class.
+        False otherwise.
     """
-    return (isinstance(obj, a_class))
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
+"""
+Module that defines if the object is an instance of a class
+that inherited (directly or indirectly) from the specified class.
+"""
+
+
 def inherits_from(obj, a_class):
     """
-    inherits_from - returns True if the object is an instance of a class
-    that inherited (directly or indirectly) from the specified class;
-    otherwise False
-    @obj: the object to check
-    @a_class: the class to check inheritance from
-    Return: True if obj is an instance of a class that inherited from a_class, else False
+    Check if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to match the type of obj against.
+
+    Returns:
+        True if the object is an instance of a class that inherited
+        (directly or indirectly) from the specified class;
+        otherwise False.
     """
-    return (issubclass(type(obj), a_class) and type(obj) is not a_class)
+    # Check if the type of obj is a subclass of a_class and ensure obj is not an instance of a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
