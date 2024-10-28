c6649904e3cdff0fe43b085cf864a84bef2677aa
ninotna <antonin@dev87.fr>
2024-10-28 14:45:22 +0100

    fix

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,6 +1,2 @@
---Write a script that creates the database hbtn_0c_0 in your MySQL server.
--- If the database hbtn_0c_0 already exists, your script should not fail
--- You are not allowed to use the SELECT or SHOW statements
--- Create the database hbtn_0c_0 if it doesn't already exist
-
+-- create the database hbtn_0c_0
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
